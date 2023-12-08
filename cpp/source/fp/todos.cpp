2290ee81abb5b2208728f729b4dab222afd3f0bd
Lakin Wecker <lakin@wecker.ca>
2023-12-08 12:35:15 -0700

    Adding more comments

--- cpp/source/fp/todos.cpp
+++ cpp/source/fp/todos.cpp
@@ -3,13 +3,23 @@
 #include <iostream>
 
 auto todo::update(Model model, Action action) -> Model {
+
+    // std::visit is a form of pattern matching for std::variant
+    // https://en.cppreference.com/w/cpp/utility/variant/visit
     return std::visit(lager::visitor{
+
+        // We have to define a function for each action type
+        // And if we miss one, the compiler will tell us
         [&](CreateTodo const &act) {
+            // Because we are using immutable data structures
+            // we can return a new copy of the model here.
             return Model{model.todos.push_back(Todo{act.name})};
         },
+
         [&](UpdateTodo const &act) {
             return Model{model.todos.set(act.pos, Todo{act.name})};
         },
+
         [&](DeleteTodo const &act) {
             return Model{model.todos.erase(act.pos)};
         }
