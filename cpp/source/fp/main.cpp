f8dcb7bbdcd49159eaf5de45e06cddadb625814c
Lakin Wecker <lakin@wecker.ca>
2023-12-08 12:33:46 -0700

    Updated with more comments

--- cpp/source/fp/main.cpp
+++ cpp/source/fp/main.cpp
@@ -7,18 +7,36 @@
 #include "parser.h"
 
 int main() {
+    // Create the data store with the initial model
     auto store = lager::make_store<todo::Action>(
         todo::Model{},
         lager::with_manual_event_loop{});
+
+    // Show the initial view
     todo::view(store.get());
+
+    // Tell lager to watch the store and when it changes
+    // to update the view
     watch(store, todo::view);
 
+    // Get a line of text
     std::string line;
     while (std::getline(std::cin, line)) {
-        parseCommand(line).map([&](auto act) {
-            store.dispatch(act);
-        }).map_error([&](auto const &err) {
-            std::cout << "Invalid command: " << err << std::endl;
-        });
+
+        // Parse the line into an action
+        parseCommand(line)
+            .map(
+                [&](auto act) {
+                    // If the action parsing was successful, dispatch it
+                    // to the store, which will call update
+                    store.dispatch(act);
+                }
+            )
+            .map_error(
+                [&](auto const &err) {
+                    // If the action parsing failed, print the error
+                    std::cout << "Invalid command: " << err << std::endl;
+                }
+            );
     }
 }
--- cpp/source/fp/parser.cpp
+++ cpp/source/fp/parser.cpp
@@ -6,21 +6,32 @@
 namespace r = ranges;
 
 auto takeFirstWord(std::string line) -> std::tuple<std::string, std::string> {
+    // Find the first word by taking all of the character we see until
+    // we find a space
     auto firstWord = line
         | r::views::take_while([](char c){return !std::isspace(c);})
         | r::to<std::string>;
+
+    // Take the rest of the line by dropping all of the characters until
+    // we find a space
     auto theRest = line
         | r::views::drop_while([](char c){return !std::isspace(c);})
         | r::views::drop_while([](char c){return std::isspace(c);})
         | r::to<std::string>;
+
+    // Return the two values as a tuple
     return std::make_tuple(firstWord, theRest);
 }
 
 auto strToSizeT(std::string const & str) -> tl::expected<size_t, std::string> {
+    // Because many of the C++ standard library functions throw exceptions
+    // we will need to manually convert these into tl::expected values
     try {
         return std::stoul(str);
+
     } catch (std::out_of_range const & e) {
         return tl::unexpected{std::format("Could not convert {} to size_t [out of range]", str)};
+
     } catch (std::invalid_argument const & e) {
         return tl::unexpected{std::format("Could not convert {} to size_t [invalid argument]", str)};
     }
@@ -35,14 +46,24 @@ auto parseCommand(std::string line) -> tl::expected<todo::Action, std::string> {
 
     if (command == "create") {
         return todo::CreateTodo{theRest};
+
     } else if (command == "edit") {
         auto [indexStr, name] = takeFirstWord(theRest);
         return strToSizeT(indexStr)
-            .map([&](size_t index){return todo::UpdateTodo{index, name};});
+            .map(
+                [&](size_t index) {
+                    return todo::UpdateTodo{index, name};
+                }
+            );
+
     } else if (command == "delete") {
         auto [indexStr, _] = takeFirstWord(theRest);
         return strToSizeT(indexStr)
-            .map([&](size_t index){return todo::DeleteTodo{index};});
+            .map(
+                [&](size_t index) {
+                    return todo::DeleteTodo{index};
+                }
+            );
     }
     return tl::unexpected{std::format("Unknown command: {}", command)};
 }
